252c7ec1893cb9c03c82bf5733ed226f12e942c8
Ladghem-Chikouche Said <said_ladghem-chikouche@ens.univ-artois.fr>
2019-02-26 12:51:25 +0100

    Adding the Next button And the scoring system

--- client/src/Quizz.js
+++ client/src/Quizz.js
@@ -3,14 +3,20 @@ import Home from "./Home.js";
 import {quizzes} from "./examples";
 import {HTTP_SERVER_PORT_PICTURES} from "./constants";
 
+
+
+
+
+
+
 class Question extends Component{
-
-
     render(){
 
 
         return (
             <div>
+                <form onSubmit={e => this.props.nextQuestion(e)}>
+
                 <h3>{this.props.q.question}</h3>
                 <ul>
                 {this.props.q.txtAnswers.map( x =>{
@@ -22,22 +28,89 @@ class Question extends Component{
 
                 })}
                 </ul>
+                    <input type="submit"  value="Next question"/>
+                </form>
             </div>
         );
     }
 
 }
+
 class Quizz extends Component{
     constructor(props) {
         super(props);
         this.quizz = quizzes.filter(q=> q._uid == this.props.match.params.id)[0];
-        this.state = {current : 1};
+        this.state = {
+            current : 0,
+            score : 0
+
+        };
+        this.NextQuestion = this.NextQuestion.bind(this);
     }
+
+    isEquivalent(a, b) {
+        console.log("A");
+        // Create arrays of property names
+        if (a.length != b.length) {
+            return false;
+        }
+        console.log("B");
+
+        for (var i = 0; i < a.length; i++) {
+
+            if (a[i] !== b[i]) {
+                return false;
+            }
+        }
+
+        console.log("C");
+
+        // If we made it this far, objects
+        // are considered equivalent
+        return true;
+    }
+
+
+
+    NextQuestion(e) {
+            let choices = [];
+            for(let i=0;i<e.target.elements.length;i++){
+                if(e.target.elements[i].checked){
+                    choices.push(i);
+                }
+            }
+
+
+            if(this.isEquivalent(choices,this.quizz.questions[this.state.current].solutions)){
+                let newScore = this.state.score + this.quizz.questions[this.state.current].points;
+                console.log(newScore);
+                this.setState({score : newScore});
+                console.log(newScore);
+
+            }
+
+            e.preventDefault();
+            let Newcurr = this.state.current + 1;
+
+            this.setState({current : Newcurr});
+            console.log(choices);
+
+
+    }
+
     render(){
+        if(this.state.current == this.quizz.questions.length){
+
+            return (
+                <div>
+                    Fin du quizz : score : {this.state.score}
+                </div>
+            )
+        }
         return (
         <div>
             {this.quizz.name}
-            <Question q={this.quizz.questions[this.state.current]} />
+            <Question q={this.quizz.questions[this.state.current]} nextQuestion={this.NextQuestion}/>
         </div>
         )
     }
